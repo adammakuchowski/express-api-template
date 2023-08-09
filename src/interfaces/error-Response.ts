3a9405b39525feb76f981716657a551d4af3473b
Adam Makuchowski <maku971@gmail.com>
2023-08-09 06:42:11 +0200

    update error types

--- src/interfaces/error-Response.ts
+++ src/interfaces/error-Response.ts
@@ -1,4 +1,4 @@
-import {MessageResponse} from './MessageResponse'
+import {MessageResponse} from './message-Response'
 
 export interface ErrorResponse extends MessageResponse {
   stack?: any;
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response} from 'express'
-import {ErrorResponse} from '../interfaces/ErrorResponse'
+import {ErrorResponse} from '../interfaces/error-Response'
 
 const errorHandler = (
   err: Error,
