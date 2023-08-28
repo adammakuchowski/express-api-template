a9c64929a9ba69c199fae224b8d357da2c508db0
Adam Makuchowski <maku971@gmail.com>
2023-08-28 06:31:27 +0200

    update app initiation process

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-import express, {Application, Request, Response} from 'express'
+import express, {
+  Request,
+  Response,
+} from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import cors from 'cors'
@@ -8,35 +11,25 @@ import loggerConfig from './configs/winstonConfig'
 import sampleRouter from './api/routes/sampleRouter'
 import errorHandler from './middlewares/errorHandler'
 import notFound from './middlewares/notFoundHandler'
+import {connectDB} from './db/db'
 
 export const logger = winston.createLogger(loggerConfig)
 
-const setupMiddlewares = (app: Application) => {
-  app.use(morgan('dev'))
-  app.use(helmet())
-  app.use(cors(corsOptions))
-  app.use(express.json())
-}
+connectDB()
+const app = express()
 
-const setupErrorHandling = (app: Application) => {
-  app.use(notFound)
-  app.use(errorHandler)
-}
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(express.json())
 
-const createApp = () => {
-  const app = express()
-  
-  setupMiddlewares(app)
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Every day you must ask yourself: Did you do enough?')
+})
 
-  app.get('/', (req: Request, res: Response): void => {
-    res.send('Every day you must ask yourself: Did you do enough?')
-  })
+app.use('/sampleRouter', sampleRouter)
 
-  app.use('/sampleRouter', sampleRouter)
+app.use(notFound)
+app.use(errorHandler)
 
-  setupErrorHandling(app)
-
-  return app
-}
-
-export default createApp
+export default app
